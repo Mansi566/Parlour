4d5a797904fbacc924592e7b2dcf35e022ab70b0
Mansi566 <mansitejani1110@gmail.com>
2026-01-22 12:58:04 -0500

    change port

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const dbURI = process.env.MONGO_URI || "mongodb://localhost:27017/my_database";
 const app = express();
 app.use(cors()); // This allows your React app to talk to the server
 app.use(express.json());
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // 1. Connect to Local MongoDB
 mongoose
