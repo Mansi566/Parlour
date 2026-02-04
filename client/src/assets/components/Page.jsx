ff2f11da093a4751cb0303e56157687b6b344112
Mansi566 <mansitejani1110@gmail.com>
2026-02-04 13:01:17 -0500

    image change is background

--- client/src/assets/components/Page.jsx
+++ client/src/assets/components/Page.jsx
@@ -156,7 +156,7 @@ function Page() {
         <div className="w-full">
           <img
             className="position-relative w-full h-full"
-            src="/dist/hero-section.png"
+            src="/public/hero-section.png"
             alt="Hero Section"
             style={{
               objectFit: "cover",
