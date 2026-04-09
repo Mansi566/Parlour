2a10d702c70c4fc7430810005a8d19b0027db6d7
Mansi566 <mansitejani1110@gmail.com>
2026-04-09 10:20:08 -0500

    .env file create

--- server/server.js
+++ server/server.js
@@ -181,11 +181,11 @@ app.use(express.json());
 
 // MySQL Connection
 const db = mysql.createConnection({
-  host: "sql.freedb.tech",
-  user: "freedb_MANSI",
-  password: "w8afE2g8VbpyU$S",
-  database: "freedb_Parlour",
-  port: 3306
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306, // Add port if needed
 });
 
 db.connect((err) => {
